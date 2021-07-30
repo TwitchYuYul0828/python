744a1cc8b20fcebafee18dcfc5d746704311d101
a01020253567 <87740736+a01020253567@users.noreply.github.com>
2021-07-30 11:04:59 +0900

    Update 20210728 미완성.py

--- 20210728 미완성.py
+++ 20210728 미완성.py
@@ -1,4 +1,4 @@
-'''
+
 print("bmi 계산기입니다.")
 weight = int(input("몸무게를 입력해주세요."))
 height = int(input("키를 입력해주세요."))
@@ -9,7 +9,7 @@ if bmi <= 18.5:
    print("저체중이시네요.")
 elif 18.5 < bmi < 23:
     print("정상 몸무게입니다.")
-'''
+      
 score : int(input("점수를 입력하세요."))
 if score >= 90:
        print("수")
