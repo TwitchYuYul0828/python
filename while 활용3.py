2cab81eddf7211dfe89169e5606584b9a5d0d5d4
a01020253567 <87740736+a01020253567@users.noreply.github.com>
2021-08-11 12:24:17 +0900

    20210811 while 활용3

--- while 활용3.py
+++ while 활용3.py
@@ -22,7 +22,38 @@ print("입력한 자료의 개수 : %d" %cnt)
 print("입력한 자료의 값 : %d" %sum_)
 print("입력한 자료의 평균 : %.2f" %avg)
 
-#map = 인덱스의 값을 순차적으로 입력하게 해줌
+#map = list에 인덱스값을 순차적으로 들어갈수있게해줌.
 
+cnt = 0
+sum_ = 0
+num = input("점수를 입력해주세요.").split()
+num = list(map(int, num))
+
+while True:
+    sum_ = sum_ + num[cnt]
+    if (num[cnt] >= 100) :
+         break
+    cnt = cnt + 1
+avg = sum_ / cnt
+print("입력한 자료의 합은 %d입니다." %sum_)
+print("입력한 자료의 합은 %.1f입니다." %avg)
+
+
+cnt = 0
+sum_ = 0
+odd_cnt = 0
+num = list(map(int, input("정수를 입력해주세요.").split()))
+
+while True:
+  if num[cnt] == 0:
+      break
+  if (num[cnt] % 2 == 1):
+     sum_ = sum_ + num[cnt]
+     odd_cnt = odd_cnt + 1
+  cnt = cnt + 1
+
+avg = sum_ / cnt
+print("입력한 자료의 합 : %d" %sum_)
+print("입력한 자료의 평균 : %d" %avg)
 
       
